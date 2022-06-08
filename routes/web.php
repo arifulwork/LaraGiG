4a139db368f5203580938c00c2aeb92a76d7ae61
Md Ariful Islam <m.ariful@graduate.utm.my>
2022-06-08 15:13:39 +0600

    routing and response

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/route', function () {
+    return ("Hello World");   // routing
+});
+
+Route::get('/response', function () {
+    return response ("Hello World", 200)
+    ->header('Content-Type', 'text/plain')
+    ->header('Ariful', 'Islam'); // response
+});
